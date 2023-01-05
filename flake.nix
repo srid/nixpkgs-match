813a3e7c92b634e6464e1628d63e0ef6f1a81426
Sridhar Ratnakumar <srid@srid.ca>
2023-01-05 18:07:51 +0530

    Use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,6 +1,16 @@
 {
   description = "Update your nixpkgs flake input to the same rev as that of the given flake";
-  outputs = { self, ... }: {
-    herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { self', lib, config, pkgs, ... }: { };
+
+      # CI configuration
+      flake.herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
+    };
 }
