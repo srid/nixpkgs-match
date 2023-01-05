681488921af6e05ded9b33e74f8e72af32e45c8f
Sridhar Ratnakumar <srid@srid.ca>
2023-01-05 18:14:08 +0530

    Initial implementation and README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # nixpkgs-match
+
 Update your nixpkgs flake input to the same rev as that of the given flake
+
+## Usage
+
+``` sh
+nix run github:srid/nixpkgs-match -- github:srid/haskell-template
+```
+
+This will update your local `flake.lock`'s "nixpkgs" input up until the revision of the "nixpkgs" input of the given flake (github:srid/haskell-template).
+
+## Motivation
+
+Instead of updating the unstable nixpkgs willy-nilly, it may be better to do the same in a canonical place, and then update other repos to the same matching nixpkgs.
--- flake.nix
+++ flake.nix
@@ -8,7 +8,33 @@
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { self', lib, config, pkgs, ... }: { };
+      perSystem = { self', lib, config, pkgs, ... }: {
+        packages =
+          let
+            # Spit out the nixpkgs rev pinned by the given flake.
+            nixpkgs-rev = pkgs.writeShellApplication {
+              name = "nixpkgs-rev";
+              text = ''
+                NIXPKGS=$(nix flake metadata --json "$1" | jq -r .locks.nodes.root.inputs.nixpkgs)
+                nix flake metadata --json "$1" | \
+                  jq -r .locks.nodes."$NIXPKGS".locked.rev
+              '';
+            };
+
+            nixpkgs-match = pkgs.writeShellApplication {
+              name = "nixpkgs-match";
+              text = ''
+                REV=$(${lib.getExe nixpkgs-rev} "$1")
+                set -x
+                nix flake lock --update-input nixpkgs --override-input nixpkgs github:nixos/nixpkgs/"$REV"
+              '';
+            };
+          in
+          {
+            inherit nixpkgs-match;
+            default = nixpkgs-match;
+          };
+      };
 
       # CI configuration
       flake.herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
